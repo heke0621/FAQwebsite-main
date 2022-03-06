6fc267e70a5cabe18e422b25386715460c3d2fbb
heke0621 <heke0621@gmail.com>
2022-03-06 13:31:09 +0800

    改善PDF瀏覽器行為

--- FAQ company/w3_include_html.js
+++ FAQ company/w3_include_html.js
@@ -9,7 +9,7 @@ function includeHTML() {
         if (file) {
             /* Make an HTTP request using the attribute value as the file name: */
             xhttp = new XMLHttpRequest();
-            xhttp.onreadystatechange = function () {
+            xhttp.onreadystatechange = function() {
                 if (this.readyState == 4) {
                     if (this.status == 200) { elmnt.innerHTML = this.responseText; }
                     if (this.status == 404) { elmnt.innerHTML = "Page not found."; }
@@ -26,8 +26,87 @@ function includeHTML() {
     }
 }
 
+const url = 'https://gosubarpublic.blob.core.windows.net/static/2021/12/13/1815/upload/171345_dfdfdf3e-11f8-49be-a695-689bb879d8ca.pdf'
+let pdfDoc = null,
+    pageNum = 1,
+    pageIsRendering = false,
+    pageNumIsPending = null;
+
+const scale = 1.5,
+    canvas = document.querySelector('#pdf-rander'),
+    ctx = canvas.getContext('2d');
+
+const renderPage = num => {
+    pageIsRendering = true;
+    //get page
+    pdfDoc.getPage(num).then(page => {
+        const viewport = page.getViewport({ scale });
+        canvas.height = viewport.height;
+        canvas.width = viewport.width;
+        const renderCtx = {
+            canvasContext: ctx,
+            viewport
+        }
+        page.render(renderCtx).promise.then(() => {
+            pageIsRendering = false;
+            if (pageNumIsPending !== null) {
+                renderPage(pageNumIsPending);
+                pageNumIsPending = null;
+            }
+        });
+        document.querySelector('#page-num').textContent = num;
+    });
+};
+
+//cheak for pages rendering
+const queueRenderPage = num => {
+        if (pageIsRendering) {
+            pageNumIsPending = num;
+        } else {
+            renderPage(num);
+        }
+    }
+    //上一頁
+const showPrevPage = () => {
+        if (pageNum <= 1) {
+            return;
+        }
+        pageNum--;
+        queueRenderPage(pageNum);
+    }
+    //下一頁
+const showNextPage = () => {
+    if (pageNum >= pdfDoc.numPages) {
+        return;
+    }
+    pageNum++;
+    queueRenderPage(pageNum);
+}
+
+//get Doc
+pdfjsLib.getDocument(url).promise.then(pdfDoc_ => {
+        pdfDoc = pdfDoc_;
+        document.querySelector('#page-count').textContent = pdfDoc.numPages;
+        renderPage(pageNum)
+    })
+    .catch(err => {
+        //顯示錯誤訊息
+        const div = document.createElement('div');
+        div.className = 'error';
+        div.appendChild(document.createTextNode(err.message));
+        document.querySelector('main').insertBefore(div, canvas);
+        //移除topbar
+        document.querySelector('.top-bar').style.display = 'none';
+    });
+
+//PDF瀏覽器按鈕事件
+document.querySelector('#prev-page').addEventListener('click', showPrevPage);
+document.querySelector('#next-page').addEventListener('click', showNextPage);
+
 $(function() {
-    if( location.pathname == '/join_member.html') { 
-        $('#nav-link').addClass('active'); 
-    };
-  });
+    if (location.pathname == '/FAQ%20company/join_member.html') {
+        $('#1').addClass('active');
+    } else if (location.pathname == '/FAQ%20company/award.html') {
+        $('#2').addClass('active');
+    }
+});
